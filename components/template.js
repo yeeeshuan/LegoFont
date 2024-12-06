52bb6911ccccbf9482702dd1339b4832ad5044fa
nahtehuang <ehuang@zendaconsulting.com>
2024-12-06 16:48:58 -0500

    initial commit

---
+++ components/template.js
@@ -0,0 +1,42 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+
+export default function template(type, placeholder, setPlaceholder) {
+
+  const handleInputChange = (e) => {
+    setPlaceholder(e.target.value.slice(-1));
+  };
+
+  return (
+    <div>
+        <h1 className={styles.subtitle}>Kit of Parts</h1>
+        <div className={styles.parts}>
+          <textarea
+            readOnly
+            disabled="disabled"
+            className={styles.element}
+            placeholder={type}
+          ></textarea>
+        </div>
+        <div className={styles.type}>
+          <div className={styles.typeLeft}>
+            <h1 className={styles.subtitle}>Type Here</h1>
+            <textarea 
+            onChange={handleInputChange} // Attach the change handler
+            className={styles.element1}>
+            </textarea>
+          </div>
+          <div className={styles.typeRight}>
+            <h1 className={styles.subtitle}>Canvas</h1>
+            <textarea 
+              readOnly
+              disabled="disabled"
+              className={styles.element2}
+              placeholder={placeholder}> 
+            </textarea>
+          </div>
+        </div>
+    </div>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+
+import template from '../components/template';
+
+
+
+export default function Home() {
+  const [placeholder, setPlaceholder] = useState(''); // State to hold the placeholder value
+  const [duckie, setDuckie] = useState(true); // State to hold the placeholder value
+
+  const click = (bool) => {
+    setDuckie(bool)
+    setPlaceholder(''); 
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+        <button 
+        className={duckie ? styles.btnClicked : styles.btn}
+        onClick={() => click(true)}>Duckie</button>
+        <button 
+        className={!duckie ? styles.btnClicked : styles.btn}
+        onClick={() => click(false)}>Frog</button>
+
+        {duckie? template("duckie", placeholder, setPlaceholder) : template("frog", placeholder, setPlaceholder)}
+      </main>
+    </div>
+  );
+}
